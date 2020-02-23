4823b7a3467160ac866194e8dbb34ee48ea2acda
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-23 14:11:07 +0100

    Show the version of roave-backward-compatibility-check

--- gh-action/entrypoint.sh
+++ gh-action/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-sh -c "composer install && /composer/vendor/bin/roave-backward-compatibility-check $*"
+sh -c "composer install && /composer/vendor/bin/roave-backward-compatibility-check --version && /composer/vendor/bin/roave-backward-compatibility-check $*"
 
