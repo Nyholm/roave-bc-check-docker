ffeb567e7573beac10e8efda29f7e0a0a91bf1a0
Nyholm <tobias.nyholm@gmail.com>
2020-02-24 16:54:15 +0100

    minir

--- gh-action/entrypoint.sh
+++ gh-action/entrypoint.sh
@@ -5,7 +5,7 @@ git remote remove bc_origin_https  >/dev/null 2>&1 || true
 git remote add bc_origin_https $REPO
 
 git fetch bc_origin_https --tags || true
-composer install --no-scripts --no-progress --quiet --no-dev
+composer install --no-scripts --no-progress --quiet
 /composer/vendor/bin/roave-backward-compatibility-check --version
 /composer/vendor/bin/roave-backward-compatibility-check $*
 
